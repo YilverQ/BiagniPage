30df3c849ef4c38eed6a1df5847bfc2859af5ce3
YilverQ <yilver0906@gmail.com>
2024-12-22 15:50:52 -0400

    Bugs arreglados

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from app.models import Company
+from app import db
+from datetime import datetime, timedelta
 
 auth = Blueprint('auth', __name__)
 
@@ -9,6 +12,10 @@ admin_user = {
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    company = Company.query.first()
+    if not company:
+        create_company()
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -22,3 +29,28 @@ def login():
             flash('Credenciales Incorrectas', 'danger')
     
     return render_template('auth/login.html')
+
+
+def create_company():
+    # Diccionario con los datos de la compañía
+    company_data = {
+        'name': 'Mi Empresa',
+        'wallpaper': 'app/static/img/defaul-wallpaper.png',
+        'picture_profile': 'app/static/img/default-profile.jpg',
+        'description': 'Descripción de mi empresa.',
+        'facebook_link': '#',
+        'instagram_link': '#',
+        'whatsapp_link': '#',
+        'other_link': '#',
+        'created_at': datetime.utcnow(),
+        'updated_at': datetime.utcnow()
+    }
+
+    # Crear una nueva instancia de Company con los datos del diccionario
+    new_company = Company(**company_data)
+
+    # Agregar la compañía a la base de datos
+    db.session.add(new_company)
+    db.session.commit()
+
+    flash('Compañía creada exitosamente', 'success')
--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
-    app.run(host='192.168.89.238', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
