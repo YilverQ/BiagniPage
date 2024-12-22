5706e124cfe4194ebb340caf7fb375158b1f12cb
YilverQ <yilver0906@gmail.com>
2024-12-22 15:29:43 -0400

    Carpeta de Migraciones ignorada

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     SECRET_KEY = secrets.token_hex(16)
 
     # Configuración de la base de datos
-    SQLALCHEMY_DATABASE_URI = 'postgresql://main:root@localhost/biagni'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://biagni:1234root@biagni.mysql.pythonanywhere-services.com/products_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
