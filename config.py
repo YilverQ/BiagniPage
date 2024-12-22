92e1d519a8f9bf7baea3f99587c088be413d83dc
YilverQ <yilver0906@gmail.com>
2024-12-22 15:56:26 -0400

    Configuración de mysql para el servidor

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ class Config:
     SECRET_KEY = secrets.token_hex(16)
 
     # Configuración de la base de datos
-    SQLALCHEMY_DATABASE_URI = 'postgresql://biagni:1234root@biagni.mysql.pythonanywhere-services.com/products_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://biagni:1234root@biagni.mysql.pythonanywhere-services.com/biagni$products_db'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://main:root@localhost/biagni'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
