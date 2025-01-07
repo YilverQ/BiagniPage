9cea6b2b6599e97c5c700f445f8ce6e40281b877
YilverQ <yilver0906@gmail.com>
2025-01-07 19:24:46 -0400

    Bugs con Biagni

--- app/blueprints/home.py
+++ app/blueprints/home.py
@@ -4,7 +4,7 @@ from app.models import Product, Company
 
 home = Blueprint('home', __name__, url_prefix='/')
 
-#Tienda Oficial
+# Tienda Oficial
 @home.route('/', methods=['GET'])
 def market():
     company = Company.query.first()
@@ -15,8 +15,14 @@ def market():
     # Obtener el número de página desde la URL (por defecto será 1 si no se proporciona)
     page = request.args.get('page', 1, type=int)
 
+    # Obtener el término de búsqueda desde la URL (si lo hay)
+    search = request.args.get('search', '', type=str)
+
     # Consultar y filtrar productos por nombre (si se proporciona un término de búsqueda)
     query = Product.query.filter(Product.availability == True).order_by(Product.updated_at.desc())
+
+    if search:
+        query = query.filter(Product.name.ilike(f'%{search}%'))  # Filtrado por nombre, insensible a mayúsculas
 
     # Realizar la paginación
     products = query.paginate(page=page, per_page=per_page, error_out=False)
@@ -25,4 +31,5 @@ def market():
         'home/index.html',
         company=company,
         products=products,
+        search=search
     )
